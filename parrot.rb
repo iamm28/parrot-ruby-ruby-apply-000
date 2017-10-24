b8786d907dbcde7d829b8b2ca9cc096c76bbee3e
Alison Mackay <alisonmackay246@gmail.com>
2017-10-24 19:09:49 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot(*phrase, str = "Squawk!")  
+def parrot(str = "Squawk!")  
   puts str
   return str
 end
